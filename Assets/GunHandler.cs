e6a4ea4efddcf569bb6b0acb4cddefd39d5d3336
PringleA <80553394+PringleA@users.noreply.github.com>
2023-03-10 11:49:31 +0000

    Enemy and Gun
    Added base for enemy, gun and cone visibilty

---
+++ Assets/GunHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunHandler : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+	}
+
+    private void Update()
+    {
+        
+    }
+}
